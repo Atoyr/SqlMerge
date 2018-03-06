65cc7e250d8e633532f22270333df9fcdba09b72
Atoyr <lessironglance@gmail.com>
2018-03-06 12:45:55 +0900

    feat:Initialize Bootstrapper

---
+++ SQLMerge/Bootstrapper.cs
@@ -0,0 +1,17 @@
+﻿using Prism.Unity;
+using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SQLMerge
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell() => this.Container.Resolve<Views.Shell>();
+        protected override void InitializeShell() => ((Window)this.Shell).Show();
+    }
+}
