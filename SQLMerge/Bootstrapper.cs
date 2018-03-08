77be34a1f4f9d17e1e7cc74c740f0121331ca848
Atoyr <lessironglance@gmail.com>
2018-03-08 13:19:44 +0900

    Windowレイアウトの修正
    * ステータスバーを実装
    * feat:regionを設定
    * feat:メニューのデザイン変更

--- SQLMerge/Bootstrapper.cs
+++ SQLMerge/Bootstrapper.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using Prism.Modularity;
+using SQLMerge.Modules;
 
 namespace SQLMerge
 {
@@ -13,5 +15,15 @@ namespace SQLMerge
     {
         protected override DependencyObject CreateShell() => this.Container.Resolve<Views.Shell>();
         protected override void InitializeShell() => ((Window)this.Shell).Show();
+
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();
+
+            var catalog = (ModuleCatalog)this.ModuleCatalog;
+            catalog.AddModule(typeof(StatusBarModule));
+            catalog.AddModule(typeof(MenuModule));
+            catalog.AddModule(typeof(MainModule));
+        }
     }
 }
