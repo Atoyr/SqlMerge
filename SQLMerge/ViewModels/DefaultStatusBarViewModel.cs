e71042b8d7d15a361ca616a4c20add36dcbba2fc
Atoyr <lessironglance@gmail.com>
2018-03-13 09:58:53 +0900

    feat:ステータスバーにステータスメッセージを表示する

---
+++ SQLMerge/ViewModels/DefaultStatusBarViewModel.cs
@@ -0,0 +1,29 @@
+﻿using SQLMerge.EventAggregator;
+using SQLMerge.EventAggregator.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLMerge.ViewModels
+{
+    public class DefaultStatusBarViewModel : UnityBindableBase
+    {
+        private string _StatusText = string.Empty;
+        public string StatusText
+        {
+            get => _StatusText;
+            set => SetProperty(ref _StatusText, value);
+        }
+
+        public DefaultStatusBarViewModel()
+        {
+            Subscriber<StatusBarMessageChangeEvent, StatusBarMessageValue>.Subscribe(x => StatusText = x.Value);
+        }
+        ~DefaultStatusBarViewModel()
+        {
+            Subscriber<StatusBarMessageChangeEvent, StatusBarMessageValue>.Unsubscribe(x => StatusText = x.Value);
+        }
+    }
+}
