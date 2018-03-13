bf8511f53876f44595734d30f78cbe8e46ea1e80
Atoyr <lessironglance@gmail.com>
2018-03-13 10:04:20 +0900

    feat:デバッグ画面に遷移した時にデバッグ画面と表示するよう修正

--- SQLMerge/Debugger/ViewModles/DebugViewModel.cs
+++ SQLMerge/Debugger/ViewModles/DebugViewModel.cs
@@ -1,4 +1,7 @@
-﻿using SQLMerge.ViewModels;
+﻿using Prism.Regions;
+using SQLMerge.EventAggregator;
+using SQLMerge.EventAggregator.Events;
+using SQLMerge.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,19 +12,15 @@ namespace SQLMerge.Debugger.ViewModels
 {
     public class DebugViewModel : UnityBindableBase
     {
-        //private ThemeSamplesViewModel _ThemeSamplesViewModel;
-        //public ThemeSamplesViewModel ThemeSamplesViewModel
-        //{
-        //    get => _ThemeSamplesViewModel;
-        //    set
-        //    {
-        //        SetProperty(ref _ThemeSamplesViewModel, value);
-        //    }
-        //}
-
-        //public DebugViewModel()
-        //{
-        //    ThemeSamplesViewModel = new ThemeSamplesViewModel();
-        //}
+        public override void OnNavigatedFrom(NavigationContext navigationContext)
+        {
+            base.OnNavigatedFrom(navigationContext);
+            Publisher<StatusBarMessageChangeEvent, StatusBarMessageValue>.Publish(new StatusBarMessageValue { Value = "" });
+        }
+        public override void OnNavigatedTo(NavigationContext navigationContext)
+        {
+            base.OnNavigatedTo(navigationContext);
+            Publisher<StatusBarMessageChangeEvent, StatusBarMessageValue>.Publish(new StatusBarMessageValue { Value = "Debug" });
+        }
     }
 }
