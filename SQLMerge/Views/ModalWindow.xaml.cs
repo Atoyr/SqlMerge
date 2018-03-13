3fe887d236b654479030212c0125520498217c3c
Atoyr <lessironglance@gmail.com>
2018-03-13 09:19:35 +0900

    bug:ModalWindowの継承元が異なっていたため修正

--- SQLMerge/Views/ModalWindow.xaml.cs
+++ SQLMerge/Views/ModalWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MetroRadiance.UI.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,7 +18,7 @@ namespace SQLMerge.Views
     /// <summary>
     /// ModalWindow.xaml の相互作用ロジック
     /// </summary>
-    public partial class ModalWindow : Window
+    public partial class ModalWindow : MetroWindow
     {
         public ModalWindow()
         {
