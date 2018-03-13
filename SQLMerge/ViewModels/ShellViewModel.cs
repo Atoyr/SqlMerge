a566c18d0850a073174392101d600eafd7686bf0
Atoyr <lessironglance@gmail.com>
2018-03-13 09:15:54 +0900

    feat:UnityBindableBaseの実装

--- SQLMerge/ViewModels/ShellViewModel.cs
+++ SQLMerge/ViewModels/ShellViewModel.cs
@@ -1,6 +1,5 @@
 ﻿using MetroRadiance.UI;
 using Prism.Events;
-using Prism.Mvvm;
 using SQLMerge.EventAggregator.Events;
 using System;
 using System.Collections.Generic;
@@ -10,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace SQLMerge.ViewModels
 {
-    public class ShellViewModel : BindableBase
+    public class ShellViewModel : UnityBindableBase
     {
     }
 }
---
+++ SQLMerge/ViewModels/UnityBindableBase.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Practices.Unity;
+using Prism.Events;
+using Prism.Mvvm;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLMerge.ViewModels
+{
+    public abstract class UnityBindableBase : BindableBase, INavigationAware
+    {
+        /// <summary>
+        /// DIコンテナを取得します
+        /// コンストラクタ実行後に登録するため、コンストラクタ内で設定する場合は別途コンストラクタに引数を受け取って処理してください
+        /// </summary>
+        [Dependency]
+        public IUnityContainer Container { get; set; }
+
+        /// <summary>
+        /// DIコンテナからEventAggregatorを登録します
+        /// コンストラクタ実行後に登録するため、コンストラクタ内で設定する場合は別途コンストラクタに引数を受け取って処理してください
+        /// </summary>
+        [Dependency]
+        public IEventAggregator EventAggregator { get; set; }
+
+        public virtual bool IsNavigationTarget(NavigationContext navigationContext) => true;
+
+        /// <summary>
+        /// 画面から遷移する時に実行
+        /// </summary>
+        /// <param name="navigationContext"></param>
+        public virtual void OnNavigatedFrom(NavigationContext navigationContext) { }
+
+        /// <summary>
+        /// 画面から遷移してきた時に実行
+        /// </summary>
+        /// <param name="navigationContext"></param>
+        public virtual void OnNavigatedTo(NavigationContext navigationContext) { }
+    }
+}
