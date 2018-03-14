64ad8474c376c5fc8edf40ba769d3d81f282fa98
Atoyr <lessironglance@gmail.com>
2018-03-14 17:47:41 +0900

    feat:SQL用テストクラス追加

---
+++ SQLMerge.Test/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SQLMerge.Common.Sql;
+using static System.Console;
+
+namespace SQLMerge.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var sqlExecuter = new SqlExecuter();
+            sqlExecuter.IsWindowsAuthentication = true;
+            sqlExecuter.CanConnect();
+            var hoge = sqlExecuter.ExecuteSql("SELECT * FROM sys.object");
+            foreach(var row in hoge)
+            {
+                foreach(var col in row)
+                {
+                    Write(col.Value);
+                }
+                WriteLine("");
+            }
+        }
+    }
+}
